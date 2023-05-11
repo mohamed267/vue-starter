17b24e54744e9a772a79e4b028a3be1e01d619e5
mohamed267 <m.rabahsidhoum@esi-sba.dz>
2023-05-12 00:19:10 +0100

    feature : create-vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export default {
+    data(){
+      return(
+        {
+          message: "here is going our message"
+        }
+      )
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <p>
+      {{ message }}
+    </p>
+  </div>
+</template>
+
+
+<docs>
+  
+</docs>
